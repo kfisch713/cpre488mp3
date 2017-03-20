c4a5ff1e0566c5b01d6fdb7285f64e8473b10015
Fischer <kfisch13@iastate.edu>
2017-03-20 13:51:03 -0500

    rebuilt hardware and BOOT.bin

--- drivers/camera_test.c
+++ drivers/camera_test.c
@@ -9,7 +9,7 @@
 // from device tree
 #define BTN_ADDRESS 0x41200000
 #define SW_ADDRESS  0x41240000
-#define FRM_BUFF1	0x107E9000
+#define FRM_BUFF1	0x10000000
 
 //#define FRM_BUFF2   0x103F4800
 //#define FRM_BUFF3   0x107E9000
@@ -59,10 +59,15 @@ int main() {
     exit(1);
   }
   
-  int dev_mem_ptr = open("/dev/mem", O_RDONLY);
+  int dev_mem_ptr = open("/dev/mem", O_RDWR);
+  if (dev_mem_ptr == -1) {
+    perror("Couldn't open file: %m");
+    exit(1);
+  }
+  
   BTNs = mmap(NULL, 1, PROT_READ, MAP_SHARED, dev_mem_ptr, BTN_ADDRESS);
   SWs = mmap(NULL, 1, PROT_READ, MAP_SHARED, dev_mem_ptr, SW_ADDRESS);
-  IMAGE_BUFF1 = mmap(NULL, HEIGHT * WIDTH * sizeof(*IMAGE_BUFF1), PROT_READ, MAP_SHARED, dev_mem_ptr, FRM_BUFF1);
+  IMAGE_BUFF1 = mmap(NULL, HEIGHT * WIDTH * sizeof(*IMAGE_BUFF1), PROT_READ | PROT_WRITE, MAP_SHARED, dev_mem_ptr, FRM_BUFF1);
   //IMAGE_BUFF2 = mmap(NULL, HEIGHT * WIDTH * sizeof(*IMAGE_BUFF2), PROT_READ, MAP_SHARED, dev_mem_ptr, FRM_BUFF2);
   //IMAGE_BUFF3 = mmap(NULL, HEIGHT * WIDTH * sizeof(*IMAGE_BUFF3), PROT_READ, MAP_SHARED, dev_mem_ptr, FRM_BUFF3);
   
@@ -80,9 +85,14 @@ int main() {
 										  IMAGE_BUFF2[(WIDTH * HEIGHT/2)], 
 										  IMAGE_BUFF3[(WIDTH * HEIGHT/2)]);
 										  */
-	printf("Center Pixel = %x\n", IMAGE_BUFF1[(WIDTH * HEIGHT/2)]);									  
+	//printf("Center Pixel = %x\n", IMAGE_BUFF1[(WIDTH * HEIGHT/2)]);									  
     fflush(stdout);
 	usleep(duration);
+	
+	IMAGE_BUFF1[WIDTH * HEIGHT/2] = 0xFFFF;
+	IMAGE_BUFF1[WIDTH * HEIGHT/2] = 0x0000;
+	IMAGE_BUFF1[WIDTH * HEIGHT/2 + 1] = 0xFFFF;
+	IMAGE_BUFF1[WIDTH * HEIGHT/2 + 1] = 0x0000;
   }
 	
   
--- sw/zynq_fsbl/Debug/src/main.d
+++ sw/zynq_fsbl/Debug/src/main.d
@@ -4,12 +4,11 @@ src/main.d: ../src/main.c ../src/fsbl.h \
  ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
  ../../system_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
  ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
- ../../system_bsp/ps7_cortexa9_0/include/xil_printf.h \
  ../../system_bsp/ps7_cortexa9_0/include/xparameters.h \
  ../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
- ../../system_bsp/ps7_cortexa9_0/include/xparameters.h \
  ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
- ../../system_bsp/ps7_cortexa9_0/include/xil_printf.h ../src/pcap.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters.h ../src/pcap.h \
  ../../system_bsp/ps7_cortexa9_0/include/xdevcfg.h \
  ../../system_bsp/ps7_cortexa9_0/include/xdevcfg_hw.h \
  ../../system_bsp/ps7_cortexa9_0/include/xil_io.h \
@@ -35,17 +34,15 @@ src/main.d: ../src/main.c ../src/fsbl.h \
 
 ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
 
-../../system_bsp/ps7_cortexa9_0/include/xil_printf.h:
-
 ../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
 
 ../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
 
-../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
-
 ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
 
 ../../system_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
 
 ../src/pcap.h:
 
